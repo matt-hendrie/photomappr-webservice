5ba8484849489459eedd8c7d0d7ab2d7b29bfc81
GuardianLux <unholyagent@gmail.com>
2020-10-24 14:32:06 +1100

    Completed exif tagging

--- image-tag-upload.py
+++ image-tag-upload.py
@@ -3,6 +3,9 @@ import boto3
 import json
 import base64
 import random
+from PIL import Image, ExifTags
+from io import BytesIO
+
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('photomappr-storage')
@@ -13,7 +16,19 @@ def handler(event, context):
         s3_response = bucket.put_object(Key=file_path, Body=file_content)
     except Exception as e:
         raise IOError(e)
+
+    image = s3.Object(bucket_name = 'photomappr-storage', key=file_path)
+    obj_body = image.get()['Body'].read()
+    img = Image.open(BytesIO(obj_body))
+
+    exif = {
+        ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in ExifTags.TAGS
+    }
+
     return {
         'statusCode': 200,
-        'body': 'hi'
+        'body': 'hi',
+        'exif': exif
     }
