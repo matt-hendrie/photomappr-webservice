610ae475a65e317bcd2f1a37cc78a45cfbde6f5c
GuardianLux <unholyagent@gmail.com>
2020-10-24 16:14:00 +1100

    completed object tagging

--- image-tag-upload.py
+++ image-tag-upload.py
@@ -1,11 +1,28 @@
 import os
 import boto3
-import json
 import base64
 import random
 from PIL import Image, ExifTags
 from io import BytesIO
+import cv2
+import numpy as np
 
+def get_weights(weights_path):
+    weightsPath = os.getcwd() + weights_path
+    return weightsPath
+
+def get_labels(labels_path):
+    lpath = os.getcwd() + labels_path
+    labels = open(lpath).read().strip().split("\n")
+    return labels
+
+def get_config(config_path):
+    configPath = os.getcwd() + config_path
+    return configPath
+
+def load_model(configpath,weightspath):
+    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
+    return net
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('photomappr-storage')
@@ -31,8 +48,54 @@ def handler(event, context):
         if k in ExifTags.TAGS
     }
 
+    # Object detection
+    # First, convert image to numpy array
+    np_array = np.fromstring(obj_body,np.uint8)                                   
+    image_np = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    # Config paths
+    cfgpath="/opts/yolov3-tiny.cfg"
+    wpath="/opts/yolov3-tiny.weights"
+    labelsPath="/opts/coco.names"
+    CFG=get_config(cfgpath)
+    Weights=get_weights(wpath)
+    labels=get_labels(labelsPath)
+
+    # Load yolo algorithm
+    net_obj=load_model(CFG,Weights)
+
+    # Object detection within image
+    class_ids = []                                                                              
+    accuracy = []                                                                               
+         
+    lnames = net_obj.getLayerNames()                                                            
+    layers = [lnames[i[0] - 1] for i in net_obj.getUnconnectedOutLayers()]                      
+        
+    blob = cv2.dnn.blobFromImage(image_np, 0.00392, (320, 320), (0, 0, 0), True, crop=False)     
+            
+    net_obj.setInput(blob)                                                                      
+        
+    result_det = net_obj.forward(layers)                                                        
+    
+    for each_result in result_det:                                                              
+        for each in each_result:                                                                
+            scores = each[5:]                                                                   
+            class_id = np.argmax(scores)                                                        
+            confidence = scores[class_id]                                                       
+            if confidence > 0.5:                                                                
+                accuracy.append(float(confidence))                                              
+                class_ids.append(class_id)
+
+    objects = {}
+    arr = []
+    if len(class_ids) > 0:
+        for i in range(len(class_ids)):
+            objects[int(i)] = {}
+            objects[int(i)]["Label"] = labels[class_ids[i]]
+            arr.append(objects[int(i)])
+
+   
     return {
-        'statusCode': 200,
-        'body': 'hi',
-        'exif': exif
+        'exif': exif,
+        'objects': arr
     }
