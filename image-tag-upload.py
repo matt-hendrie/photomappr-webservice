26289ab9e438a31d7bb7ba212126bed330a973d5
GuardianLux <unholyagent@gmail.com>
2020-10-24 11:49:51 +1100

    image uploading to s3 via REST api complete

--- image-tag-upload.py
+++ image-tag-upload.py
@@ -2,14 +2,13 @@ import os
 import boto3
 import json
 import base64
+import random
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('photomappr-storage')
-
 def handler(event, context):
     file_content = base64.b64decode(event['content'])
-    file_path = 'test'
-    #s3 = boto3.client('s3')
+    file_path = str(random.randint(1,100)) + ".jpg" 
     try:
         s3_response = bucket.put_object(Key=file_path, Body=file_content)
     except Exception as e:
