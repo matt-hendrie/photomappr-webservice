2f80572de9ed174e1f2d8bc7b890e26e7e880573
GuardianLux <unholyagent@gmail.com>
2020-10-24 14:34:49 +1100

    Added useful comments

--- image-tag-upload.py
+++ image-tag-upload.py
@@ -10,17 +10,21 @@ from io import BytesIO
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('photomappr-storage')
 def handler(event, context):
+
+    # Get image in binary from api call, convert back and store in s3 bucket
     file_content = base64.b64decode(event['content'])
     file_path = str(random.randint(1,100)) + ".jpg" 
     try:
         s3_response = bucket.put_object(Key=file_path, Body=file_content)
     except Exception as e:
         raise IOError(e)
-
+    
+    # Load image file from bucket and read with pillow
     image = s3.Object(bucket_name = 'photomappr-storage', key=file_path)
     obj_body = image.get()['Body'].read()
     img = Image.open(BytesIO(obj_body))
 
+    # Exif tag code from https://stackoverflow.com/questions/4764932/in-python-how-do-i-read-the-exif-data-for-an-image
     exif = {
         ExifTags.TAGS[k]: v
         for k, v in img._getexif().items()
