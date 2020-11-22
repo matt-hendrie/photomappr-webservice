6a14809a2116d4d8ff87b55ed4eb1c4999ec60ea
GuardianLux <unholyagent@gmail.com>
2020-11-22 12:12:47 +1100

    Added auto-confirm for submission

---
+++ auto-confirm.py
@@ -0,0 +1,11 @@
+# Code to auto-confirm users when registering, used to make testing easier
+def lambda_handler(event, context):
+    # Confirm the user
+    event['response']['autoConfirmUser'] = True
+
+    # Set the email as verified if it is in the request
+    if 'email' in event['request']['userAttributes']:
+        event['response']['autoVerifyEmail'] = True
+
+    # Return to Amazon Cognito
+    return event
